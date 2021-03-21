5673cea5350652b5f769f19db0fe796f9ebdf17d
hiancheong <hiancheong@fiscalnote.com>
2021-03-21 16:32:33 -0400

    add prodigy examples

--- mturk/generate_mturk_task.py
+++ mturk/generate_mturk_task.py
@@ -12,7 +12,12 @@ python generate_mturk_task.py --type parse_results --aws_key <AWS_KEY> --aws_sec
 
 import argparse
 import boto3
+import csv
 import json
+import os
+import os.path
+from os import path
+import time
 import random
 import xmltodict
 
@@ -33,41 +38,64 @@ def create_mturk_client(
     )
     return client
 
+def log_hit_task(hit_id, task_type, name, task_xml):
+    """
+    Create a simple csv log file of hit tasks being created locally to maintain references
+    to the hit IDs and other relevant experiment information
+    """
+    # Create directory for the generated task xml
+    if not path.isdir("experiment_xml"):
+        os.mkdir("experiment_xml")
+    with open(f"experiment_xml/{hit_id}.xml", "w+") as f:
+        f.write(task_xml)
+
+    if not path.exists("hit_experiments.csv"):
+        writer = open("hit_experiments.csv", "w+")
+        writer.write("hit_id,task_type,name,created_at\n")
+    else:
+        writer = open("hit_experiments.csv", "a")
+
+    writer.write(f"{hit_id},{task_type},{name},{time.time()}\n")
+    writer.close()
+
 def test_mturk_client(client):
     # This will return $10,000.00 in the MTurk Developer Sandbox
     print(client.get_account_balance()['AvailableBalance'])
 
-def create_ratings_task(topic_id, terms):
+def create_ratings_task(topic_name, topic_id, terms):
     ratings_html = f"""
-    <div id="ratings_question_{topic_id}">
+    <div id="ratings_question_{topic_name}_{topic_id}">
     <h3 id="instructions_header"> Please rate how related the following terms are to each other: </h3>
-    <h4 id="terms"> {",".join(terms)} </h4>
-    <input type="radio" id="not_very_related" name="ratings_{topic_id}" value="1">
+    <h4 id="terms"> {", ".join(terms)} </h4>
+    <input type="radio" id="not_very_related" name="ratings_{topic_name}_{topic_id}" value="1" required="required">
     <label for="not_very_related">Not very related</label><br>
-    <input type="radio" id="somewhat_related" name="ratings_{topic_id}" value="2">
+    <input type="radio" id="somewhat_related" name="ratings_{topic_name}_{topic_id}" value="2" required="required">
     <label for="somewhat_related">Somewhat related</label><br>
-    <input type="radio" id="very_related" name="ratings_{topic_id}" value="3">
+    <input type="radio" id="very_related" name="ratings_{topic_name}_{topic_id}" value="3" required="required">
     <label for="very_related">Very related</label>
     </div>
     """
     return ratings_html
 
-def create_intrusion_task(topic_id, terms, intruder_topic_id, intruder_term):
+def create_intrusion_task(topic_name, topic_id, terms, intruder_topic_id, intruder_term):
     
     radio_button_strings = [
         f"""
-            <input type="radio" id="{term}" name="intrusion_{topic_id}_{intruder_topic_id}" value="{term}_{idx}">
-            <label for="{term}">{term}</label>
+            <input type="radio" id="{term}" name="intrusion_{topic_name}_{topic_id}_{intruder_topic_id}" value="{term}_{idx}" required="required">
+            <label for="{term}">{term}</label><br>
         """
         for idx, term in enumerate(terms)
     ]
     # add the intruder
     radio_button_strings.append(
         f"""
-            <input type="radio" id="{intruder_term}" name="intrusion_{topic_id}_{intruder_topic_id}" value="{intruder_term}_-1">
-            <label for="{intruder_term}">{intruder_term}</label>
+            <input type="radio" id="{intruder_term}" name="intrusion_{topic_name}_{topic_id}_{intruder_topic_id}" value="{intruder_term}_-1" required="required">
+            <label for="{intruder_term}">{intruder_term}</label><br>
         """
     )
+    # Randomize the order of the terms with the intruder
+    # This ensures the intruder doesn't always occur
+    # in the same position for every question
     random.shuffle(radio_button_strings)
     intrusion_html = f"""
     <div id="intrusion_question_{topic_id}_{intruder_topic_id}">
@@ -80,8 +108,9 @@ def create_intrusion_task(topic_id, terms, intruder_topic_id, intruder_term):
 def create_intrusion_assignment(intrusion_setup):
     intrusion_strings = "".join([
         create_intrusion_task(
+            setup["topic_name"],
             setup["topic_id"],
-            setup["topic_terms"],
+            setup["terms"],
             setup["intruder_id"],
             setup["intruder_term"]
         ) for setup in intrusion_setup
@@ -114,7 +143,7 @@ def create_intrusion_assignment(intrusion_setup):
 
 def create_ratings_assignment(ratings_setup):
     ratings_strings = "".join(
-        [create_ratings_task(setup["topic_id"], setup["terms"]) for setup in ratings_setup]
+        [create_ratings_task(setup["topic_name"], setup["topic_id"], setup["terms"]) for setup in ratings_setup]
     )
     ratings_form = f"""
         <HTMLQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2011-11-11/HTMLQuestion.xsd">
@@ -141,51 +170,95 @@ def create_ratings_assignment(ratings_setup):
     """
     return ratings_form
 
-def create_intrusion_hit(client, intrusion_setup):
+def create_intrusion_hit(client, intrusion_setup, name="Topic Intrusion Task", reward=1.0, assignments=10):
+    task_xml = create_intrusion_assignment(intrusion_setup)
     new_hit = client.create_hit(
-        Title = 'Topic Intrusion',
+        Title = name,
         Description = 'Please identify which term is the least related to the other terms',
         Keywords = 'text, quick, labeling',
-        Reward = '0.15',
-        MaxAssignments = 10,
+        Reward = str(reward),
+        MaxAssignments = assignments,
         LifetimeInSeconds = 172800,
-        AssignmentDurationInSeconds = 600,
-        AutoApprovalDelayInSeconds = 14400,
-        Question = create_intrusion_assignment(intrusion_setup),
+        AssignmentDurationInSeconds = 60*30,
+        AutoApprovalDelayInSeconds = 48 * 60 * 60,
+        Question = task_xml,
+        QualificationRequirements=[
+            {
+                'QualificationTypeId': '00000000000000000071',
+                'Comparator': 'EqualTo',
+                'LocaleValues': [
+                    {
+                        'Country': 'US'
+                    }
+                ]
+            }
+        ]
     )
     print("A new HIT has been created. You can preview it here:")
     print("https://workersandbox.mturk.com/mturk/preview?groupId=" + new_hit['HIT']['HITGroupId'])
     print("HITID = " + new_hit['HIT']['HITId'] + " (Use to Get Results)")
     hit_id = new_hit['HIT']['HITId']
+    log_hit_task(hit_id, "intrusion", name, task_xml)
     return hit_id
 
-def create_ratings_hit(client, ratings_setup):
+def create_ratings_hit(client, ratings_setup, name="Topic Ratings Task", reward=1.0, assignments=10):
+    task_xml = create_ratings_assignment(ratings_setup)
     new_hit = client.create_hit(
-        Title = 'Topic Ratings',
+        Title = name,
         Description = 'Please describe how related the following sets of words are to each other',
         Keywords = 'text, quick, labeling',
-        Reward = '0.15',
-        MaxAssignments = 10,
+        Reward = str(reward),
+        MaxAssignments = assignments,
         LifetimeInSeconds = 172800,
-        AssignmentDurationInSeconds = 600,
-        AutoApprovalDelayInSeconds = 14400,
-        Question = create_ratings_assignment(ratings_setup),
+        AssignmentDurationInSeconds = 60*15,
+        AutoApprovalDelayInSeconds = 48 * 60 * 60,
+        Question = task_xml,
+        QualificationRequirements=[
+            {
+                'QualificationTypeId': '00000000000000000071',
+                'Comparator': 'EqualTo',
+                'LocaleValues': [
+                    {
+                        'Country': 'US'
+                    }
+                ]
+            }
+        ]
     )
     print("A new HIT has been created. You can preview it here:")
     print("https://workersandbox.mturk.com/mturk/preview?groupId=" + new_hit['HIT']['HITGroupId'])
     print("HITID = " + new_hit['HIT']['HITId'] + " (Use to Get Results)")
     hit_id = new_hit['HIT']['HITId']
+    log_hit_task(hit_id, "ratings", name, task_xml)
+
     return hit_id
 
 def parse_results(client, hit_id):
-    worker_results = client.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted'])
-    scores = []
+    worker_results = client.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted', 'Approved'])
+    results = []
+    print("Found {} responses.".format(len(worker_results.get("Assignments", []))))
+    print(worker_results)
     for assignment in worker_results["Assignments"]:
+        
+        result = {
+            "worker_id": assignment["WorkerId"],
+            "accept_time": assignment["AcceptTime"].isoformat(),
+	        "submit_time": assignment["SubmitTime"].isoformat(),
+            "responses": []
+        }
         assignment_dict = xmltodict.parse(assignment["Answer"])
+        
         for answer in assignment_dict["QuestionFormAnswers"]["Answer"]:
-            print(answer["QuestionIdentifier"], answer["FreeText"])
-            scores.append({"question_id":answer["QuestionIdentifier"], "answer":answer["FreeText"]})
-    return scores
+            
+            result["responses"].append({
+                "question_id": answer["QuestionIdentifier"],
+                "answer": answer["FreeText"]
+            })
+        results.append(result)
+    json.dump(results, open(f"results/{hit_id}.json", "w+"), indent=2)
+    print(json.dumps(results, indent=2))
+
+    return results
 
 
 if __name__ == "__main__":
@@ -193,13 +266,20 @@ if __name__ == "__main__":
     # Parse out the arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--prod", help="Use production MTurk endpoint", action="store_true")
-    parser.add_argument("--type", '-t', type=str, help="Select either 'ratings', 'intrusion' or 'parse_results'", required=True)
-    parser.add_argument("--aws_key", help="AWS Key", type=str, required=True)
-    parser.add_argument("--aws_secret", help="AWS Secret", type=str, required=True)
-    parser.add_argument("--topics_file", help="Filename of topics to use for generating task", type=str)
+    parser.add_argument("--type", '-t', type=str, help="Select either 'ratings', 'intrusion', 'parse_ratings'", required=True)
+    parser.add_argument("--aws_key", help="AWS Key", type=str, required=False)
+    parser.add_argument("--aws_secret", help="AWS Secret", type=str, required=False)
+    parser.add_argument("--aws_keys_file", help="Path to file with aws keys", type=str, required=False)
+    #parser.add_argument("--topics_file", help="Filename of topics to use for generating task", type=str)
     parser.add_argument("--hit_id", help="HIT ID for parsing results", type=str)
 
     args = parser.parse_args()
+
+    if args.aws_keys_file:
+        with open(args.aws_keys_file) as f:
+            lines = [line.replace("\n", "") for line in f.readlines()]
+            args.aws_key = lines[0]
+            args.aws_secret = lines[1]
 
     endpoint = "https://mturk-requester.us-east-1.amazonaws.com" if args.prod \
         else "https://mturk-requester-sandbox.us-east-1.amazonaws.com"
@@ -214,27 +294,27 @@ if __name__ == "__main__":
 
     # TODO - Add actual topics file parsing
     ratings_setup = [
-        {"topic_id": 0, "terms": ['dog','cat','rabbit','snake','fish']},
-        {"topic_id": 1, "terms": ['taco','burrito','enchilada','fajitas','salsa']},
-        {"topic_id": 2, "terms": ['andrew','riley','claire','annelise','eric']}
+        {"topic_id": 0, "terms": ['dog','cat','rabbit','snake','fish'], "topic_name": "Food"},
+        {"topic_id": 1, "terms": ['taco','burrito','enchilada','fajitas','salsa'], "topic_name": "Food"},
+        {"topic_id": 2, "terms": ['andrew','riley','claire','annelise','eric'], "topic_name": "Food"}
     ]
     
     intrusion_setup = [
         {
             "topic_id": 0,
-            "topic_terms": ['dog','cat','rabbit','snake','fish'],
+            "terms": ['dog','cat','rabbit','snake','fish'],
             "intruder_id": 10,
             "intruder_term": "apple"
         },
         {
             "topic_id": 1,
-            "topic_terms": ['taco','burrito','enchilada','fajitas','salsa'],
+            "terms": ['taco','burrito','enchilada','fajitas','salsa'],
             "intruder_id": 20,
             "intruder_term": "mouse"
         },
         {
             "topic_id": 2,
-            "topic_terms": ['andrew','riley','claire','annelise','eric'],
+            "terms": ['andrew','riley','claire','annelise','eric'],
             "intruder_id": 30,
             "intruder_term": "vlad"
         }
@@ -245,6 +325,7 @@ if __name__ == "__main__":
     elif args.type == "intrusion":
         hit_id = create_ratings_hit(client, intrusion_setup)
     elif args.type == "parse_results":
+        print("Parsing Results...")
         parse_results(client, args.hit_id)
     else:
         raise Exception(f"Invalid specified task type: {args.type}")
---
+++ prodigy/README.md
@@ -0,0 +1,4 @@
+
+`prodigy textcat.manual example_ratings ./example_ratings_data.jsonl --label 'Not Very Related,Somewhat Related,Very Related' --exclusive`
+
+`prodigy mark example_intrusion example_intrusion_data.jsonl --view-id choice`
